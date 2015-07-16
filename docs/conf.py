29ab8a58dd2ffad6c22706f8b8c79069ccd88bae
Jaime Marquínez Ferrándiz <jaime.marquinez.ferrandiz@gmail.com>
2015-07-16 22:13:50 +0200

    docs: Remove html_static_path from conf.py
    It's unused and not included in the repo

--- docs/conf.py
+++ docs/conf.py
@@ -59,11 +59,6 @@ intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 # a list of builtin themes.
 html_theme = 'alabaster'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'pyucldoc'
 
